75dcfc6b011f9ca3819cbcee0dbeaab22c154c02
Student User <student@workstation.lab.example.com>
2021-12-24 07:26:06 -0500

    Deleted

--- DO288/labs/expose-image/create-secret.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /usr/local/etc/ocp4.config
-
-oc create secret generic quayio \
---from-file .dockerconfigjson=${XDG_RUNTIME_DIR}/containers/auth.json \
---type kubernetes.io/dockerconfigjson
-
--- DO288/labs/external-registry/create-secret.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-oc create secret generic quayio \
-    --from-file .dockerconfigjson=${XDG_RUNTIME_DIR}/containers/auth.json \
-    --type=kubernetes.io/dockerconfigjson
-
